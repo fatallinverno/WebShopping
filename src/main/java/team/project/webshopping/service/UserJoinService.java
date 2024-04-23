fdefc499f4d074cc82c1e5e849c8cdf75d759d2a
린베르노 <fatal_linverno@naver.com>
2024-04-23 22:39:30 +0900

    회원가입 Service 추가

---
+++ src/main/java/team/project/webshopping/service/UserJoinService.java
@@ -0,0 +1,15 @@
+package team.project.webshopping.service;
+
+import org.apache.catalina.User;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class UserJoinService {
+
+    public void userJoin(User user) throws IOException {
+
+    }
+
+}
