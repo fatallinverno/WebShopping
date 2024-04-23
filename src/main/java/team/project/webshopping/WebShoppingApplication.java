3138d6160f330b29345cde07d69b017aa757f8e6
roh <rohilkie94@gmail.com>
2024-04-23 09:22:26 +0900

    SecurityAutoConfiguration exclude 옵션 추가

--- src/main/java/team/project/webshopping/WebShoppingApplication.java
+++ src/main/java/team/project/webshopping/WebShoppingApplication.java
@@ -2,8 +2,9 @@ package team.project.webshopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class WebShoppingApplication {
 
 	public static void main(String[] args) {
