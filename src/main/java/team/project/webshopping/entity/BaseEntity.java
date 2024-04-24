5c1b4a7c523bad2d114d0880f8a9f64267a56c23
린베르노 <fatal_linverno@naver.com>
2024-04-24 21:47:42 +0900

    생성, 수정 날짜 자동화

--- src/main/java/team/project/webshopping/entity/BaseEntity.java
+++ src/main/java/team/project/webshopping/entity/BaseEntity.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 public class BaseEntity {
+
     @CreationTimestamp
     @Column(updatable = false)
     private LocalDateTime createdTime;
@@ -21,4 +22,5 @@ public class BaseEntity {
     @UpdateTimestamp
     @Column(insertable = false)
     private LocalDateTime updatedTime;
+
 }
