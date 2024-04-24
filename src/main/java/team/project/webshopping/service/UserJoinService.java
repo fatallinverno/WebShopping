fee1e6b1f37950543377bd978380023164b80e30
린베르노 <fatal_linverno@naver.com>
2024-04-24 21:50:35 +0900

    회원가입 서비스 생성

--- src/main/java/team/project/webshopping/service/UserJoinService.java
+++ src/main/java/team/project/webshopping/service/UserJoinService.java
@@ -2,13 +2,14 @@ package team.project.webshopping.service;
 
 import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
+import team.project.webshopping.dto.UserJoinDTO;
 
 import java.io.IOException;
 
 @Service
 public class UserJoinService {
 
-    public void userJoin(User user) throws IOException {
+    public void userJoin(UserJoinDTO userJoinDTO) throws IOException {
 
     }
 
