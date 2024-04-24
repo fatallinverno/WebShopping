759a5978edb4f55684d5394d4ea36e9e9f98c082
린베르노 <fatal_linverno@naver.com>
2024-04-24 21:49:49 +0900

    회원가입DTO 추가

--- src/main/java/team/project/webshopping/dto/UserJoinDTO.java
+++ src/main/java/team/project/webshopping/dto/UserJoinDTO.java
@@ -1,10 +1,36 @@
 package team.project.webshopping.dto;
 
-
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class UserJoinDTO {
+
+    private Long id;
+    private String password;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+    private String role;
+    private LocalDateTime userJoinCreatedTime;
+    private LocalDateTime userJoinUpdatedTime;
+
+    public void userJoinDTO(Long id, String password, String name, String phone, String email, String address, String role, LocalDateTime userJoinCreatedTime){
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.role = "ROLE_USER";
+    }
+
+    public static UserJoinDTO toDTO(UserJoinDTO userJoinDTO){
+        UserJoinDTO userDto = new UserJoinDTO();
+
+        return userJoinDTO;
+    }
+
 }
